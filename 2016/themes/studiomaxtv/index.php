c71f60d7b45acecf6458d111adb8c56d75148319
Gean <Gean@SERVIDOR>
2016-05-18 23:57:57 -0400

    Commit - v.1.6  - Fatiar Layout

--- 2016/themes/studiomaxtv/index.php
+++ 2016/themes/studiomaxtv/index.php
@@ -6,12 +6,78 @@
 
     </section>
     <section class="wrapper-busca">
-        
+        <div class="busca-tit">Buscar</div>
+        <div class="busca-form">
+            <form action="">
+                <input type="text" name="s" class="busca-input" placeholder="Digite o que deseja buscar. Ex. Tribuna Livre...">
+                <button type="submit" name="searchSubmit" class="busca-btn"><span class="fa fa-search fa-2x"></span> Buscar</button>
+            </form>
+        </div>
     </section>
     <section class="wrapper-videos">
-        
+        <div class="wrapper-category">
+            <div class="vin vin-green">
+                <span class="vin-title">Últimos</span>
+                <span class="vin-btn vin-btn-green">VER MAIS</span>
+            </div>
+            <div class="video-group">
+                <?php for ($v = 1; $v <= 5; $v++): ?>
+                    <div class="video-box">
+                        <div class="video-thumb"></div>
+                        <div class="video-title">Titulo do video aparecerá aqui para visualização</div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+
+        <div class="wrapper-category">
+            <div class="vin vin-red">
+                <span class="vin-title">Política</span>
+                <span class="vin-btn vin-btn-red">VER MAIS</span>
+            </div>
+            <div class="video-group">
+                <?php for ($v = 1; $v <= 5; $v++): ?>
+                    <div class="video-box">
+                        <div class="video-thumb"></div>
+                        <div class="video-title">Titulo do video aparecerá aqui para visualização</div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+
+        <div class="wrapper-category">
+            <div class="vin vin-blue">
+                <span class="vin-title">Esporte</span>
+                <span class="vin-btn vin-btn-blue">VER MAIS</span>
+            </div>
+            <div class="video-group">
+                <?php for ($v = 1; $v <= 5; $v++): ?>
+                    <div class="video-box">
+                        <div class="video-thumb"></div>
+                        <div class="video-title">Titulo do video aparecerá aqui para visualização</div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+
+        <div class="wrapper-category">
+            <div class="vin vin-orange">
+                <span class="vin-title">Saúde</span>
+                <span class="vin-btn vin-btn-orange">VER MAIS</span>
+            </div>
+            <div class="video-group">
+                <?php for ($v = 1; $v <= 5; $v++): ?>
+                    <div class="video-box">
+                        <div class="video-thumb"></div>
+                        <div class="video-title">Titulo do video aparecerá aqui para visualização</div>
+                    </div>
+                <?php endfor; ?>
+            </div>
+        </div>
     </section>
     <section class="wrapper-newsletter">
-        
+        <div class="vin vin-purple">
+            <span class="vin-title">Newsletter</span>
+        </div>
     </section>
 </div>
